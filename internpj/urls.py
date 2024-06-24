f397fcf16c958781fd0ea6bf9433b5189737f786
nigrumdiaster <119689847+nigrumdiaster@users.noreply.github.com>
2024-06-24 14:38:26 +0700

    Change project name from "maps" to "internpj" + deleted some unused library

--- internpj/urls.py
+++ internpj/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for maps project.
+URL configuration for internpj project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
