e92135c2a866cf199fd058cadf747adf0f13519c
nigrumdiaster <nigrumdiaster@gmail.com>
2024-06-26 16:41:27 +0700

    update map + chức năng zoom tới vị trí đó bên trang bản đồ

--- maps/views.py
+++ maps/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render
 from django.views import View
 from django.core.paginator import Paginator
 from destination.models import Location
+import json
+from django.core.serializers.json import DjangoJSONEncoder
 
 
-
-
-# Create your views here.
 template_error = '404error.html'
+
 class Show_maps(View):
     template_name = 'maps/show_maps.html'
+    
     def get(self, request):
         try:
             locations = Location.objects.all().order_by('-created_at')
@@ -17,11 +18,11 @@ class Show_maps(View):
 
             page_number = request.GET.get('page')
             page_obj = paginator.get_page(page_number)
-            data = {'page_obj': page_obj}
+            locations_list = list(locations.values('name', 'latitude', 'longitude'))
+            data = {'page_obj': page_obj, 'locations_json': json.dumps(locations_list, cls=DjangoJSONEncoder)}
 
             return render(request, self.template_name, data)
     
         except Exception as e:
             print(f"Error: {e}")
             return render(request, template_error)
-
