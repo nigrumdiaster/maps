03ade7cc1b3e0f198c0c5074f14429e338c8179d
nigrumdiaster <nigrumdiaster@gmail.com>
2024-06-27 07:59:23 +0700

    Change location_detail to detail_location

--- destination/urls.py
+++ destination/urls.py
@@ -5,6 +5,6 @@ from .views import Create_location, List_location, Detail_location
 urlpatterns = [
     path('create/', Create_location.as_view(), name='create_location'),
     path('location/', List_location.as_view(), name='list_location'),
-    path('locations/<int:pk>/', Detail_location.as_view(), name='location_detail'),
+    path('locations/<int:pk>/', Detail_location.as_view(), name='detail_location'),
     # Other URL patterns for your app
 ]
