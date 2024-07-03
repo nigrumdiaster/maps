d8c7847b83af781c5da7780d157a63c6808c1ec1
quang270902 <74493768+quang270902@users.noreply.github.com>
2024-07-03 14:11:29 +0700

    update library

--- library/models.py
+++ library/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Image(models.Model):
     title = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='library/images/')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -12,7 +12,7 @@ class Image(models.Model):
 
 class Video(models.Model):
     title = models.CharField(max_length=255)
-    video = models.FileField(upload_to='videos/')
+    video = models.FileField(upload_to='library/videos/')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- library/views.py
+++ library/views.py
@@ -1,14 +1,26 @@
 # library/views.py
 
+from urllib import request
 from django.urls import reverse_lazy
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, CreateView, TemplateView
 from .models import Image, Video
+from django.core.paginator import Paginator
 from posts.models import Post
 from destination.models import LocationImage
 
 class ListImageView(ListView):
     model = Image
     template_name = 'library/list_image.html'
+
+    def list_image(request):
+    image = Image.objects.all().order_by('-created_at')
+    paginator = Paginator(image, 10)  # Hiển thị 10 bài viết trên mỗi trang
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'library/list_image.html', {'page_obj': page_obj})
 
 class AddImageView(CreateView):
     model = Image
@@ -28,6 +40,8 @@ class AddImageView(CreateView):
 
 class InheritImageView(TemplateView):
     template_name = 'library/inherit_image.html'
+    context_object_name = 'images'
+    paginate_by = 10
 
     def get(self, request, *args, **kwargs):
         # Fetch image URLs from Post and Destination models
@@ -48,3 +62,6 @@ class InheritImageView(TemplateView):
                 Image.objects.create(title='Inherited from Destination', image=image_url)
 
         return super().get(request, *args, **kwargs)
+    
+    def get_queryset(self):
+        return Image.objects.all()
